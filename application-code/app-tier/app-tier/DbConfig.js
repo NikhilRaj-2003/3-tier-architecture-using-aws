1d6a35b05b4c98ddb572730264c1215f18816815
Nikhil Raj A <nikhilsiri2003@gmail.com>
2025-06-11 21:45:53 +0530

    Add files via upload

---
+++ application-code/app-tier/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.c34og8skgbri.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'nikhil1234',
+    DB_DATABASE : 'webapp-DB'
+});
